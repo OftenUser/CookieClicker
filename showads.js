40e4baae1312421124a2516b3aa7cda3879ef6cc
OftenGuy <77411288+OftenUser@users.noreply.github.com>
2024-12-19 16:18:10 -0500

    Update showads.js

--- showads.js
+++ showads.js
@@ -1,3 +1,3 @@
-﻿//this lets us detect adblockers so we can adjust the layout in case ads aren't shown ! (we're not using this for anything weird, promise !)
-//this works because an adblocker will usually block this file from being embedded at all
-var showAds=true;
+﻿// This lets us detect adblockers so we can adjust the layout in case ads aren't shown! (We're not using this for anything weird, promise!)
+// This works because an adblocker will usually block this file from being embedded at all
+var showAds = true;
